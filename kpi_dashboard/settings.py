a453530b3f67cb8629372cb96eabfbabf7f95b12
Kartik Ahuja <kartik.ahuja2@gmail.com>
2022-03-19 01:37:47 +0530

    added gitignore

--- kpi_dashboard/settings.py
+++ kpi_dashboard/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e894*yjb-g=j#rbv(*$g6s8@v7#r#56l%f-jx6*qw0x-t)-*zh'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
